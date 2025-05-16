051edb9e775b7335af59f80145e6999e1c94b2ea
Paulo Santos <paulorfsantos17@gmail.com>
2025-05-15 21:30:09 -0300

    feat: inicialize database with SQLAlchemy

--- src/app.py
+++ src/app.py
@@ -1,8 +1,57 @@
-from flask import Flask
+import os
 
-app = Flask(__name__)
+import click
+from flask import Flask, current_app
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
 
-@app.route('/')
-def hello_world():
-  return 'Hello, World!'
-  
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
+
+
+@click.command("init-db")
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    global db
+    with current_app.app_context():
+        db.create_all()
+    click.echo("Initialized the database.")
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        SQLALCHEMY_DATABASE_URI="sqlite:///dio_bank.db",
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route("/hello")
+    def hello():
+        return "Hello, World!"
+
+    # registration of commands
+    app.cli.add_command(init_db_command)
+
+    # inilialize extenstions
+    db.init_app(app)
+
+    return app
