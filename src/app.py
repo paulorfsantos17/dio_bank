8b0122bccc9c58f616193c27a18e88abeee523b4
Paulo Santos <paulorfsantos17@gmail.com>
2025-05-23 16:05:19 -0300

    feat: add test_config

--- src/app.py
+++ src/app.py
@@ -73,7 +73,7 @@ def init_db_command():
 
 
 # Função factory para criar a aplicação Flask
-def create_app():
+def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY="dev",
